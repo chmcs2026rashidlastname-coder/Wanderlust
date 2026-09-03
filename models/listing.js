3f15261ca140770c6cc6774699e85ddfae71f1a9
Altaf <chmcs2026altaf.shaikh@gmail.com>
2026-09-04 02:28:12 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,39 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+const Review=require("./review.js")
+
+const ListingSchema=new Schema({
+    title:{
+        type:String,
+        required:true
+
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    image:{
+        filename:String,
+        url:String
+    },
+    price:Number,
+    location:String,
+    country:String,
+    reviews:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Review"
+        }
+
+    ]
+}
+
+    
+);
+ListingSchema.post("findOneAndDelete",async (listing)=>{
+    if(listing){
+    await Review.deleteMany({_id:{$in:listing.reviews}});
+    }
+})
+const Listing=mongoose.model("Listing",ListingSchema);
+module.exports=Listing;
